727dbf9181333e1cddf1d29ba44477217e51fb7f
Naim A <naim94a@gmail.com>
2016-09-06 01:49:12 +0300

    Simple setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name='vix',
+	version='1.0',
+	description='VIX binding for Python',
+	author='Naim A.',
+	author_email='naim94a@gmail.com',
+	url='https://github.com/naim94a/vix',
+	packages=['vix'],
+	package_dir={
+		'vix': 'vix',
+	},
+)
