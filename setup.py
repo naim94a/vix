497123c993517bc1702feb25eb86bcaf7646a249
Naim A <naim94a@gmail.com>
2016-09-12 01:36:16 +0300

    Update package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name='vix',
-	version='1.0.0',
+	version='1.0.1',
 	description='VIX binding for Python',
 	author='Naim A.',
 	author_email='naim94a@gmail.com',
