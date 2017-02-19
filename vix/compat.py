ee4b607bcb3a7dbf547cc35463bb4008d8199195
chrono-meter <chrono-meter@gmx.net>
2017-02-19 19:10:38 +0900

    Tab to space

--- vix/compat.py
+++ vix/compat.py
@@ -4,15 +4,15 @@ import sys
 
 
 if sys.version_info.major < 3:
-	def _bytes(data, encoding=None):
-		if encoding:
-			return str(data).encode(encoding)
-		else:
-			return str(data)
+    def _bytes(data, encoding=None):
+        if encoding:
+            return str(data).encode(encoding)
+        else:
+            return str(data)
 
-	def _str(data, encoding):
-		return str(data).decode(encoding)
+    def _str(data, encoding):
+        return str(data).decode(encoding)
 
 else:
-	_bytes = bytes
-	_str = str
+    _bytes = bytes
+    _str = str
